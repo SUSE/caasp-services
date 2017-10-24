1a645d0fb35a573dabb491dbb8293b3923e79146
Nikhil Manchanda <SlickNik@gmail.com>
2017-10-24 11:33:27 -0700

    Update all of the Portus components to use TLS
    Updated Portus, docker-registry, and nginx to use TLS to
    communicate with one another, and with the outside world.

--- docs/portus/secure/generate-certs.sh
+++ docs/portus/secure/generate-certs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 openssl genrsa -out ./secrets/portus.key 3072
-openssl req -new -x509 -key ./secrets/portus.key -sha256 -out ./secrets/portus.crt -days 9999 -config ./openssl.cnf -subj "/C=US/ST=WA/L=SUSE/O=Seattle/CN=nginx.portus.svc.cluster.local"
+openssl req -new -x509 -key ./secrets/portus.key -sha256 -out ./secrets/portus.crt -days 9999 -config ./openssl.cnf -subj "/C=US/ST=WA/L=SUSE/O=Seattle/CN=*.nip.io"
